705b96f1def824fa2f63b1e799103ce72785df58
Nathan Yost <nathan.yost@hotmail.com>
2016-10-09 16:54:57 -0400

    Minor Changes
    Made play button move towards cursor, created HowTo Folder in Assets.
    Colliders on edges of screen.

---
+++ Project-Juno/Assets/Scripts/PlayButton.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayButton : MonoBehaviour
+{
+    private float moveSpeed;
+	// Use this for initialization
+	void Start ()
+    {
+        moveSpeed = .05f;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        // Move object towards mouse cursor // Issue: mousePosition is mapped to pixel coordinate plane, not world coordinates.
+        Vector2 playButtonPosition = new Vector2(transform.position.x, transform.position.y),
+            mousePosition = new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y);
+        transform.position = Vector2.MoveTowards(playButtonPosition, mousePosition, moveSpeed);
+	}
+}
